049e750ffd865956b7ac85ffa5b7a3e107e0f1b1
NatWij <137473831+NatWij@users.noreply.github.com>
2023-07-27 18:11:54 +0800

    Adding to about page
    I have added some text and styes to the about page, also started inputting images.
    contemplated starting this whole section again but I will see how it looks when completed then decide

---
+++ JavaScript/show_hide_elements.js
@@ -0,0 +1,11 @@
+function show_hide() {
+var x = document.getElementById("showpage")
+if (x.style.visibility === "hidden") 
+{
+    x.style.visibility = "visible";
+}
+else 
+{
+    x.style.visibility = "hidden";
+}
+}
